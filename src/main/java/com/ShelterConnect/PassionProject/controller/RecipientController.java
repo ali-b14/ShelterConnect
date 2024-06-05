d6a0d8ae797649e9678850c3c55ed7d9bd655ad0
ali-b14 <alib714@gmail.com>
2024-06-04 21:02:23 -0400

    Add getMapping

--- src/main/java/com/ShelterConnect/PassionProject/controller/RecipientController.java
+++ src/main/java/com/ShelterConnect/PassionProject/controller/RecipientController.java
@@ -4,10 +4,10 @@ import com.ShelterConnect.PassionProject.entities.Recipient;
 import com.ShelterConnect.PassionProject.service.RecipientService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 //Rest controller is a convenient annotation that combines @Controller and @ResponseBody, which eliminates the need to annotate every request handling method of the controller class with the @ResponseBody annotation
 @RestController
 public class RecipientController {
@@ -22,9 +22,15 @@ public class RecipientController {
     //build create recipient REST API
     // @PostMapping handles post http requests.
     //@RequestBody annotation allows us to retrieve the request's body and automatically convert it to Java object.
-    @PostMapping("/recipients")
+    @PostMapping("/save")
     public ResponseEntity<Recipient> saveRecipient(@RequestBody Recipient recipient){
         return new ResponseEntity<Recipient>(recipientService.saveRecipient(recipient), HttpStatus.CREATED);
     }
+
+    @GetMapping("/recipients")
+    public ResponseEntity<List<Recipient>> getAllRecipients(){
+        return getAllRecipients();
+    }
+
 
 }
