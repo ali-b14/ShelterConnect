3f397929c136fdfeaa8203dbe4a8d6678e3ac8c1
ali-b14 <alib714@gmail.com>
2024-06-08 16:59:33 -0400

    Testing Springboot annotations

--- src/main/java/com/ShelterConnect/PassionProject/ShelterConnectApplication.java
+++ src/main/java/com/ShelterConnect/PassionProject/ShelterConnectApplication.java
@@ -2,11 +2,17 @@ package com.ShelterConnect.PassionProject;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 import java.sql.SQLOutput;
 
-@SpringBootApplication
+//@SpringBootApplication
+@ComponentScan
+@Configuration
+@AutoConfiguration
 public class ShelterConnectApplication {
 
 	public static void main(String[] args) {
