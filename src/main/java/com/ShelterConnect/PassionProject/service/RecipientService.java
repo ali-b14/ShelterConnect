42ad64412035169fcc0c7376472052d3fbf1443d
Ali Bangash <alib714@gmail.com>
2024-07-24 13:20:15 -0400

    test

--- src/main/java/com/ShelterConnect/PassionProject/service/RecipientService.java
+++ src/main/java/com/ShelterConnect/PassionProject/service/RecipientService.java
@@ -4,6 +4,5 @@ import com.ShelterConnect.PassionProject.entities.Recipient;
 //
 public interface RecipientService {
     Recipient saveRecipient(Recipient recipient);
-    void deleteRecipient(short id);
     void deleteRecipient(long id);
 }
